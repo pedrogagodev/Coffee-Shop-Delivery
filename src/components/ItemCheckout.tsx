3c4df56ee23880d587bac439081d45cd125a80a6
Pedro Oliveira <pedrogagodev@gmail.com>
2025-01-20 00:17:01 -0300

    feat: adds the feature to remove an item from the cart

--- src/components/ItemCheckout.tsx
+++ src/components/ItemCheckout.tsx
@@ -1,12 +1,17 @@
 import { Trash } from '@phosphor-icons/react';
 import { CoffeeQuantity } from './CoffeeQuantity';
-import { CoffeeProps } from '../contexts/CoffeeContext';
+import { CoffeeContext, CoffeeProps } from '../contexts/CoffeeContext';
+import { useContext } from 'react';
 
 interface ItemCheckoutProps {
   coffee: CoffeeProps;
 }
 
 export default function ItemCheckout({ coffee }: ItemCheckoutProps) {
+  const { handleQuantityChange } = useContext(CoffeeContext);
+  function handleRemoveCoffee() {
+    handleQuantityChange(coffee.id, 0);
+  }
   return (
     <div className="flex gap-5 border-b-2 border-base-button pb-6 pt-6">
       <img src={coffee.image} alt={coffee.name} className="h-16 w-16" />
@@ -14,7 +19,10 @@ export default function ItemCheckout({ coffee }: ItemCheckoutProps) {
         <span>{coffee.name}</span>
         <div className="flex">
           <CoffeeQuantity coffeeId={coffee.id} coffee={coffee} />
-          <button className="flex gap-1 self-center rounded-md bg-base-button p-2">
+          <button
+            className="flex gap-1 self-center rounded-md bg-base-button p-2"
+            onClick={handleRemoveCoffee}
+          >
             <Trash size={16} className="mt-1 text-my-purple" weight="bold" />
             <span>REMOVER</span>
           </button>
