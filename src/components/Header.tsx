b4cd36a4dfa30245239fbf22dfbf20af419d39c5
Pedro Oliveira <pedrogagodev@gmail.com>
2025-01-21 00:17:58 -0300

    feat: shows in the header the number of items in the cart

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,6 +9,9 @@ export function Header() {
   const { coffees } = useContext(CoffeeContext);
   const navigate = useNavigate();
   const coffeesInCart = coffees.filter((coffee) => coffee.quantity > 0);
+  const totalItemsInCart = coffeesInCart.reduce((total, coffee) => {
+    return total + coffee.quantity;
+  }, 0);
   const handleClick = (): void | Promise<void> => {
     if (coffeesInCart.length !== 0) {
       navigate('checkout');
@@ -26,9 +29,12 @@ export function Header() {
           <span className="text-purple-dark">Porto Alegre, RS</span>
         </div>
         <div
-          className="flex cursor-pointer items-center rounded-md bg-yellow-light p-2"
+          className="relative flex cursor-pointer items-center rounded-md bg-yellow-light p-2"
           onClick={handleClick}
         >
+          <div className="absolute right-0 top-0 flex h-5 w-5 -translate-y-2 translate-x-2 justify-center rounded-full bg-yellow-dark text-white">
+            {totalItemsInCart}
+          </div>
           <ShoppingCart size={22} weight="fill" className="text-yellow-dark" />
         </div>
       </div>
