58f63dbcdd7ff2fea375b72625300d37a4fb61e3
Pedro Oliveira <pedrogagodev@gmail.com>
2025-01-27 00:41:11 -0300

    Refactor: Adds responsiveness to success page

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -22,15 +22,15 @@ export function Success() {
   }
 
   return (
-    <div className="mx-40 mt-20 flex gap-24">
+    <div className="mx-10 mt-10 flex flex-col gap-24 md:mx-40 md:mt-20 md:flex-row md:gap-60">
       <div>
         <h1 className="mb-1 text-3xl font-extrabold text-yellow-dark">
           Uhu! Pedido Confirmado
         </h1>
-        <p className="mb-10 text-nowrap text-xl text-base-subtitle">
+        <p className="mb-10 text-lg text-base-subtitle md:text-xl">
           Agora é só aguardar que logo o café chegará até você
         </p>
-        <div className="w-full rounded-md bg-gradient-to-r from-my-yellow to-my-purple p-0.5">
+        <div className="mb-6 w-full rounded-md bg-gradient-to-r from-my-yellow to-my-purple p-0.5">
           <div className="roundedbr-md flex flex-col gap-8 rounded-bl-3xl rounded-tl-md rounded-tr-3xl bg-white p-10">
             <div className="flex">
               <MapPin />
